5c653eb57bb9d0241d65484fba8c2e7162c30754
GuillaumeChk <guillaume.chekhab39@gmail.com>
2021-08-28 20:49:06 +0200

    routes + models + controllers

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
+const saucesRoutes = require('./routes/sauces');
 const userRoutes = require('./routes/user');
 
 const app = express();
@@ -19,8 +20,9 @@ app.use((req, res, next) => {
     next();
   });
 
-  app.use(bodyParser.json());
+app.use(bodyParser.json());
 
- app.use('/api/auth', userRoutes);
+app.use('/api/sauces', saucesRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
--- backend/models/Things.js
+++ backend/models/Things.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 
-// const thingSchema = mongoose.Schema({
-//   title: { type: String, required: true },
-//   description: { type: String, required: true },
-//   imageUrl: { type: String, required: true },
-//   userId: { type: String, required: true },
-//   price: { type: Number, required: true },
-// });
+const thingSchema = mongoose.Schema({
+    userId: { type: String, required: true },
+    name: { type: String, required: true },
+    manufacturer: { type: String, required: true },
+    description: { type: String, required: true },
+    mainPepper: { type: String, required: true },
+    imageUrl: { type: String, required: true },
+    heat: { type: Number, required: true },
+    likes: { type: Number},
+    dislikes: { type: Number},
+    usersLiked: { type: Array},
+    usersDisliked: { type: Array}
+});
 
 module.exports = mongoose.model('Thing', thingSchema);
