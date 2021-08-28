1cc90c83a2e031666ae69765d6895247a5f8d695
GuillaumeChk <guillaume.chekhab39@gmail.com>
2021-08-28 15:44:07 +0200

    auth : signup et login

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const userRoutes = require('./routes/user');
 
 const app = express();
 
-app.use((req, res) => {
-    res.json({ message: 'Votre requête a bien été reçue !' }); 
- });
+mongoose.connect('mongodb+srv://Gui:motdepasse@cluster0.2klb8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB : réussie.'))
+  .catch(() => console.log('Connexion à MongoDB : échouée.'));
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    next();
+  });
+
+  app.use(bodyParser.json());
+
+ app.use('/api/auth', userRoutes);
 
 module.exports = app;
