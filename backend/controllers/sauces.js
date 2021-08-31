1181bb0595dec4218f4c97b39923098f11543d99
GuillaumeChk <guillaume.chekhab39@gmail.com>
2021-08-31 12:33:33 +0200

    fonctionnalité like/dislike fini

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -78,5 +78,65 @@ exports.deleteThing = (req, res, next) => {
 };
 
 exports.likeSauce = (req, res, next) => {
-
+  // console.log(req.body);
+  Thing.findOne({ _id: req.params.id })
+  .then((sauce) => { 
+    console.log("sauce: " + sauce);
+    if (req.body.like === 1) { /// like
+      // console.log(sauce.userId + req.body.userId);
+      // console.log(sauce.usersLiked.includes(req.body.userId));
+      if (!sauce.usersLiked.includes(req.body.userId)) {
+        console.log("like ajout");
+        sauce.usersLiked.push(req.body.userId);
+        sauce.likes++;
+        sauce.save()
+          .then(() => res.status(200).json({ message: 'Sauce like'}))
+          .catch(error => res.status(400).json({ error }));
+        // const sauce2 = sauce;
+        // console.log("sauce2: " + sauce2);
+        // Thing.updateOne({ _id: req.params.id }, { ...sauce2, _id: req.params.id })
+        //   .then(() => res.status(200).json({ message: 'Sauce like'}))
+        //   .catch(error => res.status(400).json({ error }));
+      }
+      else { // inutile ?
+        console.log("déjà like");
+        res.status(200).json({ message: 'Sauce deja like'})
+      }
+    }
+    else if (req.body.like === -1) { // dislike
+      if (!sauce.usersDisliked.includes(req.body.userId)) {
+        console.log("dislike ajout");
+        sauce.usersDisliked.push(req.body.userId);
+        sauce.dislikes++;
+        sauce.save()
+          .then(() => res.status(200).json({ message: 'Sauce dislike'}))
+          .catch(error => res.status(400).json({ error }));
+      }
+      else { // inutile ?
+        console.log("déjà dislike");
+        res.status(200).json({ message: 'Sauce deja dislike'})
+      }
+    }
+    else { // annulation de dis/like
+      if (sauce.usersLiked.includes(req.body.userId)) { // like
+        console.log("annulation de like");
+        sauce.likes--;
+        sauce.usersLiked.splice(sauce.usersLiked.indexOf(req.body.userId),1);
+        sauce.save()
+          .then(() => res.status(200).json({ message: 'Sauce like annulé'}))
+          .catch(error => res.status(400).json({ error }));
+      }
+      else { // dislike
+        console.log("annulation de dislike");
+        sauce.dislikes--;
+        sauce.usersDisliked.splice(sauce.usersDisliked.indexOf(req.body.userId),1);
+        sauce.save()
+          .then(() => res.status(200).json({ message: 'Sauce dislike annulé'}))
+          .catch(error => res.status(400).json({ error }));
+      }
+    }
+  }, reason => {
+    console.log(reason);
+  })
+  .catch((error) => { res.status(400).json({ error: error });});
 };
